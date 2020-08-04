86267dfb0c6213916959098c674486510bca1ccb
chetyanant <69159645+chetyanant@users.noreply.github.com>
2020-08-04 10:26:48 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,92 +1,33 @@
 <?php
 
 // URL API LINE
-$API_URL = 'https://api.line.me/v2/bot/message';
+// $API_URL = 'https://api.line.me/v2/bot/message';
 // ใส่ Channel access token (long-lived)
-$ACCESS_TOKEN = 'BCOvf1t/i6Yw4Tuh5YL1ee0i9B5i63x57X62O4Bk3ZIO0SoW6Sp5pUGl7wZOypqYv7iywr1OGuf8z5tYmKzwBwoMz4UdwT/DMO1vxa2+mVqdGlFdoDkpnAqeRw8mTGNyy3PIbn3eH5+jIW4dinIFkgdB04t89/1O/w1cDnyilFU=';
+$accessToken = 'BCOvf1t/i6Yw4Tuh5YL1ee0i9B5i63x57X62O4Bk3ZIO0SoW6Sp5pUGl7wZOypqYv7iywr1OGuf8z5tYmKzwBwoMz4UdwT/DMO1vxa2+mVqdGlFdoDkpnAqeRw8mTGNyy3PIbn3eH5+jIW4dinIFkgdB04t89/1O/w1cDnyilFU=';
 // ใส่ Channel Secret
-$CHANNEL_SECRET = 'ad758cdb84873efe137e6d24b5712732';
+// $CHANNEL_SECRET = 'ad758cdb84873efe137e6d24b5712732';
 
-// Set HEADER
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
 
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-// Get request content
-$request = file_get_contents('php://input');
-// Decode JSON to Array
-$request_array = json_decode($request, true);
+//รับข้อความจากผู้ใช้
+$message = $arrayJson['events'][0]['message']['text'];
 
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
+if($message == "สวัสดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+    replyMsg($arrayHeader,$arrayPostData);
 }
-
-if (!is_null($request_array['events'])) {
-
-	// Loop through each event
-	foreach ($request_array['events'] as $request_array) {
-    
-        // Line API send a lot of event type, we interested in message only.
-		if ($request_array['type'] == 'message') {
-
-            switch($request_array['message']['type']) {
-                
-                case 'text':
-                    // Get replyToken
-                    $replyToken = $request_array['replyToken'];
-   
-                    // Reply message
-                    $respMessage = ''. $request_array['message']['text'];
-                    /*
-                    $userText = ''. $event['message']['text'];
-                    switch($userText){
-                        case 'กำหนดสอบ':
-                            $respMessage = 'กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
-                            http://www.gongtham.net/web/news.php';
-                        break;
-
-                        case 'ใบคำร้อง':
-                            $respMessage = 'ดาวน์โหลดใบคำร้อง คลิ๊ก >>
-                            http://www.gongtham.net/web/downloads.php?cat_id=5&download_id=80';
-                        break;
-
-                    }
-                    */
-                    if($request_array['message']['text'] == "กำหนดสอบ"){
-                        $respMessage = "กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
-                        http://www.gongtham.net/web/news.php";
-
-                    }elseif($request_array['message']['text'] == "ขอใบประกาศ"){
-                        $respMessage = "ดาวน์โหลดใบคำร้อง คลิ๊ก >>
-                        http://www.gongtham.net/web/downloads.php?cat_id=5&download_id=80";
-
-                    }elseif($request_array['message']['text'] == ""){
-                        $respMessage = ''. $request_array['message']['text'];
-                    }else{
-                        $respMessage = 'ติดต่อเจ้าหน้าที่ โทร. ...';
-                    }
-                                
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                    
-                    break;
-            }
-		}
-	}
+#ตัวอย่าง Message Type "Sticker"
+else if($message == "ฝันดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "sticker";
+    $arrayPostData['messages'][0]['packageId'] = "2";
+    $arrayPostData['messages'][0]['stickerId'] = "46";
+    replyMsg($arrayHeader,$arrayPostData);
 }
-
-echo "Hello LINEBot";
