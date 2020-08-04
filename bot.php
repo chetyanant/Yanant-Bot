218ea3922d3a892e736ca2344fa4bdd21a113b03
chetyanant <69159645+chetyanant@users.noreply.github.com>
2020-08-04 11:40:07 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -28,21 +28,29 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
-if ( sizeof($request_array['events']) > 0 ) {
+if ( sizeof($request_array['events'])) {
+
+   // Loop through each event
    foreach ($request_array['events'] as $event) {
-      
-    $reply_message = '';
-    $reply_token = $event['replyToken'];
-    $text = $event['message']['text'];
-    $data = [
-        'replyToken' => $reply_token,
-        'messages' => [['type' => 'Hi', 
-        'text' => 'Hello' ]]
-    ];
+
+    // Line API send a lot of event type, we interested in message only.
+	if ($event['type'] == 'message') {
+        switch($event['message']['type']) { 
+            case 'text':
+                // Get replyToken
+                $replyToken = $event['replyToken'];
+                // Reply message
+                $respMessage = ''. $event['message']['text'];
+
+                if($event['message']['text'] == "กำหนดสอบ"){
+                    $respMessage = "กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
+                    http://www.gongtham.net/web/news.php";
+
+                }
 	  
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
-      echo "Result: ".$send_result."\r\n";
+                $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+                $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+                echo "Result: ".$send_result."\r\n";
     }
 }
 echo "OK";
