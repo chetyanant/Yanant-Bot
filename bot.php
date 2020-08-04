16d946d917c20986cc3b970334b3cfcc72631c64
chetyanant <69159645+chetyanant@users.noreply.github.com>
2020-08-04 10:52:41 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -32,13 +32,26 @@ if ( sizeof($request_array['events']) > 0 ) {
    foreach ($request_array['events'] as $event) {
       
       $reply_token = $event['replyToken'];
-      $reply_message = '';
-      $text = $event['message']['text'];
-      $data = [
-         'replyToken' => $reply_token,
-         'messages' => [['type' => 'text', 'text' => $text ]]
+      $reply_message = ''. $event['message']['text'];
+    //   $text = $event['message']['text'];
+    //   $data = [
+    //      'replyToken' => $reply_token,
+    //      'messages' => [['type' => 'text', 'text' => $text ]]
 
-      ];
+    //   ];
+    if($event['message']['text'] == "กำหนดสอบ"){
+        $reply_message = "กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
+        http://www.gongtham.net/web/news.php";
+
+    }elseif($event['message']['text'] == "ขอใบประกาศ"){
+        $reply_message = "ดาวน์โหลดใบคำร้อง คลิ๊ก >>
+        http://www.gongtham.net/web/downloads.php?cat_id=5&download_id=80";
+
+    }elseif($event['message']['text'] == ""){
+        $reply_message = ''. $event['message']['text'];
+    }else{
+        $reply_message = 'ติดต่อเจ้าหน้าที่ โทร. ...';
+    }
 	  
       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
       $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
