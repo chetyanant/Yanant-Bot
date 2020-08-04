527ade5ceb8796be55988745344b343b806b1081
chetyanant <69159645+chetyanant@users.noreply.github.com>
2020-08-04 09:48:57 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,78 +1,47 @@
 <?php
+
 // URL API LINE
 $API_URL = 'https://api.line.me/v2/bot/message';
-
-require_once('./nextflow-reserve/vendor/autoload.php');
-
-// Namespace
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-
 // ใส่ Channel access token (long-lived)
 $ACCESS_TOKEN = 'BCOvf1t/i6Yw4Tuh5YL1ee0i9B5i63x57X62O4Bk3ZIO0SoW6Sp5pUGl7wZOypqYv7iywr1OGuf8z5tYmKzwBwoMz4UdwT/DMO1vxa2+mVqdGlFdoDkpnAqeRw8mTGNyy3PIbn3eH5+jIW4dinIFkgdB04t89/1O/w1cDnyilFU=';
 // ใส่ Channel Secret
 $CHANNEL_SECRET = 'ad758cdb84873efe137e6d24b5712732';
 
-// Get message from Line API
-$content = file_get_contents('php://input');
-$events = json_decode($content, true);
+// Set HEADER
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+// Get request content
+$request = file_get_contents('php://input');
+// Decode JSON to Array
+$request_array = json_decode($request, true);
 
-if (!is_null($events['events'])) {
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-    
-        // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-
-            switch($event['message']['type']) {
-                
-                case 'text':
-                    // Get replyToken
-                    $replyToken = $event['replyToken'];
-   
-                    // Reply message
-                    $respMessage = ''. $event['message']['text'];
-                    /*
-                    $userText = ''. $event['message']['text'];
-                    switch($userText){
-                        case 'กำหนดสอบ':
-                            $respMessage = 'กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
-                            http://www.gongtham.net/web/news.php';
-                        break;
-
-                        case 'ใบคำร้อง':
-                            $respMessage = 'ดาวน์โหลดใบคำร้อง คลิ๊ก >>
-                            http://www.gongtham.net/web/downloads.php?cat_id=5&download_id=80';
-                        break;
-
-                    }
-                    */
-                    if($event['message']['text'] == "กำหนดสอบ"){
-                        $respMessage = "กำหนดสอบธรรมสนามหลวง คลิ๊ก >>
-                        http://www.gongtham.net/web/news.php";
-
-                    }elseif($event['message']['text'] == "ขอใบประกาศ"){
-                        $respMessage = "ดาวน์โหลดใบคำร้อง คลิ๊ก >>
-                        http://www.gongtham.net/web/downloads.php?cat_id=5&download_id=80";
-
-                    }elseif($event['message']['text'] == ""){
-                        $respMessage = ''. $event['message']['text'];
-                    }else{
-                        $respMessage = 'ติดต่อเจ้าหน้าที่ โทร. ...';
-                    }
-                                
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                    
-                    break;
-            }
-		}
-	}
+    return $result;
 }
 
-echo "Hello LINEBot";
+if ( sizeof($request_array['events']) > 0 ) {
+   foreach ($request_array['events'] as $event) {
+      
+      $reply_message = '';
+      $reply_token = $event['replyToken'];
+      $text = $event['message']['text'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [['type' => 'text', 'text' => $text ]]
+      ];
+	  
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+    }
+}
+echo "OK";
