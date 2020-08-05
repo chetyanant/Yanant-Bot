20a2a6ec27fcb3e188f8670db4676e9645d4102b
chetyanant <69159645+chetyanant@users.noreply.github.com>
2020-08-05 13:20:51 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -27,28 +27,21 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
-if ( sizeof($request_array['events'])) {
+if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
-        
-        if ($event['type'] == 'message') {
-            switch($event['message']['type']){
-                case 'text':
-                    $reply_token = $event['replyToken'];
-                    $reply_message = '';.$event['message']['text'];
-            }
-        }
+       
+       $reply_message = '';
+       $reply_token = $event['replyToken'];
+       $text = $event['message']['text'];
+       $data = [
+          'replyToken' => $reply_token,
+          'messages' => [['type' => 'text', 'text' => $text ]]
+       ];
 
+       if($event['message']['text'] == "Hi"){
+        $reply_message = "Hi5";
+       }
 
-
-
-
-    //    $reply_message = '';
-    //    $reply_token = $event['replyToken'];
-    //    $text = $event['message']['text'];
-    //    $data = [
-    //       'replyToken' => $reply_token,
-    //       'messages' => [['type' => 'text', 'text' => $text ]]
-    //    ];
        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
        $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
        echo "Result: ".$send_result."\r\n";
